57d3efa0678fd750298bbf1f17867e8f15a3864e
YolaMsk <masukuyolanda@gmail.com>
2024-10-22 21:24:23 +0200

    Adding api funtion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+  let apiKey = "adc5tcbc786490db6do6b9402a16f53f";
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
