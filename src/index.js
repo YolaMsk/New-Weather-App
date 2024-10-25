bce3f63beb186454c6ff7d6043fa603ed033e830
YolaMsk <masukuyolanda@gmail.com>
2024-10-25 09:47:03 +0200

    java script fixing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ function refreshWeather(response) {
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
-
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
@@ -13,16 +12,27 @@ function refreshWeather(response) {
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
 
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000); // Assuming `response.data.time` is in seconds
-
-  // Get the day name and format the time
-  let options = { weekday: 'long' }; // For day name
-  let dayName = date.toLocaleDateString(undefined, options);
-  let timeString = date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
-
-  timeElement.innerHTML = `${dayName} ${timeString}`;
+  let date = new Date(response.data.time);
+  timeElement.innerHTML = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 
   let iconElement = document.querySelector("#icon img");
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
 }
+
+function searchCity(city) {
+  let apiKey = "adc5tcbc786490db6do6b9402a16f53f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
