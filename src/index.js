b579379e4771ef98b0907a4ba4eaf7f9a8cfc5d5
YolaMsk <masukuyolanda@gmail.com>
2024-10-25 10:54:15 +0200

    hours and minutes added to date element

--- src/index.js
+++ src/index.js
@@ -5,26 +5,23 @@ function refreshWeather(response) {
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
-  
+
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
-  
-  let timezoneOffset = response.data.timezone;
-  let localDate = new Date(Date.now() + timezoneOffset * 1000);
 
-  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  let dayName = days[localDate.getUTCDay()];
-  let hours = localDate.getUTCHours().toString().padStart(2, '0');
-  let minutes = localDate.getUTCMinutes().toString().padStart(2, '0'); 
-  
   let timeElement = document.querySelector("#time");
-  timeElement.innerHTML = `${dayName} ${hours}:${minutes}`;
+let date = new Date(response.data.time);
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let dayName = days[date.getDay()];
+let hours = date.getHours().toString().padStart(2, '0');
+let minutes = date.getMinutes().toString().padStart(2, '0');
+timeElement.innerHTML = `${dayName} ${hours}:${minutes}`;
+
 
   let iconElement = document.querySelector("#icon img");
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
 }
-
