e2f52d30ef04b5d10fd70486bc841e33dd3c825c
YolaMsk <masukuyolanda@gmail.com>
2024-10-25 20:24:40 +0200

    final code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
+function formatDate(date) {
+  let minutes = date.getMinutes().toString().padStart(2, '0');
+  let hours = date.getHours().toString().padStart(2, '0');
+  let dayIndex = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  
+  let formattedDay = days[dayIndex];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
@@ -12,16 +34,25 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
 
-  let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time);
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let dayName = days[date.getDay()];
-let hours = date.getHours().toString().padStart(2, '0');
-let minutes = date.getMinutes().toString().padStart(2, '0');
-timeElement.innerHTML = `${dayName} ${hours}:${minutes}`;
-
-
   let iconElement = document.querySelector("#icon img");
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
+
+  let currentDateElement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  currentDateElement.innerHTML = formatDate(currentDate);
 }
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
